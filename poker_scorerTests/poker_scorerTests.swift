487a42c2ab5076c733db42458ae469d35c38ce5a
hisato <hisato@hisatonoMacBook-Air.local>
2025-08-24 09:17:34 +0900

    Initial commit - iOS Poker Scorer app

---
+++ poker_scorerTests/poker_scorerTests.swift
@@ -0,0 +1,16 @@
+//
+//  poker_scorerTests.swift
+//  poker_scorerTests
+//
+//  Created by hisato on 2025/08/24.
+//
+
+import Testing
+
+struct poker_scorerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
